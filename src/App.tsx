d77afcf181d5214efda8c19e77f902423ada36a9
ishiyamatokimune <ishiyama.tokimune.io3@is.naist.jp>
2023-02-13 13:23:13 +0900

    add project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import Node from "./Node";
+
+function App() {
+    // const [count, setCount] = useState(0)
+    // let node_list = [
+    //     {x: 1, y: 2, title: 'Node 1'},
+    //     {x: 2, y: 2, title: 'Node 2'},
+    //     {x: 2, y: 1, title: 'Node 1'},
+    //     {x: 3, y: 2, title: 'Node 1'},
+    //     {x: 2, y: 3, title: 'Node 1'},
+    // ]
+
+    return (
+        <div className="App">
+            <Node/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Node.tsx
@@ -0,0 +1,60 @@
+import './Node.css'
+
+import React, {useState} from 'react'
+
+function Node() {
+    const default_node_list = new Map([
+        ['5-5', {x: 5, y: 5, title: 'テーマ', isActive: true}],
+    ])
+
+    const [node_list, setNodeList] = useState(default_node_list)
+    const [isExistNode, setIsExistNode] = useState(new Set(default_node_list.keys()))
+
+    function activateNode(key: string) {
+        let node = node_list.get(key)
+        if (node) {
+            node.isActive = true
+            setNodeList(node_list)
+        }
+        expandNode(key)
+    }
+
+    function expandNode(key: string) {
+        let dir: number[][] = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        let newTitles: string[] = ['なぜ？', '同じような事例', 'ということは？', '過去は？']
+        let newNodeList = new Map()
+        let tmp_node = node_list.get(key)
+        
+        let x = tmp_node?.x ? tmp_node.x : 0
+        let y = tmp_node?.y ? tmp_node.y : 0
+
+        if (x === 0 || y === 0) {
+            console.log('エラー発生');
+        }
+
+        for (let i = 0; i < dir.length; i++) {
+            let xx = x + dir[i][0]
+            let yy = y + dir[i][1]
+
+            if (x > 0 && y > 0 && !isExistNode.has(`${xx}-${yy}`)) {
+                newNodeList.set(`${xx}-${yy}`, {x: xx, y: yy, title: newTitles[i], isActive: false})
+                setIsExistNode(isExistNode.add(`${xx}-${yy}`))
+            }
+        }
+        setNodeList(new Map([...node_list, ...newNodeList]))
+    }
+
+    return (
+        <div className="wrapper">
+            {[...node_list].map(([key, value]) => <div
+                className={`node ${value.isActive ? 'node-active' : 'node-inactive'}`}
+                onClick={() => (activateNode(key))}
+                key={key}
+                style={{gridColumn: value.x, gridRow: value.y}}
+                contentEditable={true} suppressContentEditableWarning={true}
+            >{value.title}</div>)
+            }
+        </div>)
+}
+
+export default Node
